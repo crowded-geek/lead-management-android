c75762d4790e8ac41193f0b9733c269faf0c8734
Lint <44368997+C0derLint@users.noreply.github.com>
2018-12-04 23:27:02 +0530

    Added Firbase sign out preference in settings menu. (#172)

--- app/src/main/java/com/community/jboss/leadmanagement/main/MainActivity.java
+++ app/src/main/java/com/community/jboss/leadmanagement/main/MainActivity.java
@@ -38,10 +38,8 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
@@ -285,27 +283,23 @@ public class MainActivity extends BaseActivity
         showProgressDialog();
         AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
         mAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "signInWithCredential:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI(user);
-                        } else {
-                            if(task.getException() != null) {
-                                Toast.makeText(MainActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
-                            }else {
-                                Toast.makeText(MainActivity.this, "Unknown error occurred, please try again.", Toast.LENGTH_SHORT).show();
-                            }
-                            updateUI(null);
-                        }
-                        // [START_EXCLUDE]
-                        hideProgressDialog();
-                        // [END_EXCLUDE]
-                    }
-                });
+            .addOnCompleteListener(this, task -> {
+                if (task.isSuccessful()) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success");
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    updateUI(user);
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.getException());
+                    //Snackbar.make(findViewById(R.id.main_layout), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Authentication failed", Toast.LENGTH_SHORT ).show();
+                    updateUI(null);
+                }
+                // [START_EXCLUDE]
+                hideProgressDialog();
+                // [END_EXCLUDE]
+            });
     }
 
 
@@ -331,7 +325,6 @@ public class MainActivity extends BaseActivity
                     }
                 });
     }
-
 
     private void updateUI(FirebaseUser user) {
         hideProgressDialog();
@@ -360,7 +353,7 @@ public class MainActivity extends BaseActivity
             }
 
         } else {
-            Toast.makeText(getApplicationContext(), "Signed out", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), "Not signed in", Toast.LENGTH_SHORT).show();
 
             mStatusTextView.setText(R.string.app_desc);
             mDetailTextView.setText(R.string.app_name);
